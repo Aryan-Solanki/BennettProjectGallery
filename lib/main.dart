cfd92dd36394faca0c79d5f23760eed8e7576a9c
Rishika Solanki <37494973+Aryan-Solanki@users.noreply.github.com>
2021-12-27 00:27:08 +0530

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -73,8 +73,128 @@ class _MyHomePageState extends State<MyHomePage> {
             ListView(
               children: <Widget>[
                 Container(
-                  height: 600,
+                  height: MediaQuery.of(context).size.height,
                   color: Colors.transparent,
+                ),
+                Container(
+                  color: Color(0xff210002),
+                  width: double.infinity,
+                  padding: EdgeInsets.only(top: 70),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Parallax In",
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontFamily: "MontSerrat-ExtraLight",
+                            letterSpacing: 1.8,
+                            color: Color(0xffffaf00)),
+                      ),
+                      Text(
+                        "Flutter",
+                        style: TextStyle(
+                            fontSize: 51,
+                            fontFamily: "MontSerrat-Regular",
+                            letterSpacing: 1.8,
+                            color: Color(0xffffaf00)),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: 190,
+                        child: Divider(
+                          height: 1,
+                          color: Color(0xffffaf00),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Made By",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontFamily: "Montserrat-Extralight",
+                          letterSpacing: 1.3,
+                          color: Color(0xffffaf00),
+                        ),
+                      ),
+                      Text(
+                        "The CS Guy",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "Montserrat-Regular",
+                          letterSpacing: 1.8,
+                          color: Color(0xffffaf00),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Color(0xff210002),
+                  width: double.infinity,
+                  padding: EdgeInsets.only(top: 70),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Parallax In",
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontFamily: "MontSerrat-ExtraLight",
+                            letterSpacing: 1.8,
+                            color: Color(0xffffaf00)),
+                      ),
+                      Text(
+                        "Flutter",
+                        style: TextStyle(
+                            fontSize: 51,
+                            fontFamily: "MontSerrat-Regular",
+                            letterSpacing: 1.8,
+                            color: Color(0xffffaf00)),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: 190,
+                        child: Divider(
+                          height: 1,
+                          color: Color(0xffffaf00),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Made By",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontFamily: "Montserrat-Extralight",
+                          letterSpacing: 1.3,
+                          color: Color(0xffffaf00),
+                        ),
+                      ),
+                      Text(
+                        "The CS Guy",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "Montserrat-Regular",
+                          letterSpacing: 1.8,
+                          color: Color(0xffffaf00),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      )
+                    ],
+                  ),
                 ),
                 Container(
                   color: Color(0xff210002),
