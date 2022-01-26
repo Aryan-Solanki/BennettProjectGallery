75b904fdccb7fa82cbfb5758222a3df2393c9908
AKSHAT RASTOGI <akshat28vivek@hotmail.com>
2022-01-26 12:37:30 +0530

    ..

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_parallax/main.dart';
+import 'package:bennettprojectgallery/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
