c9aec0ed6838b7f78a89a26d9e3f75472dd06ef3
Rishika Solanki <37494973+Aryan-Solanki@users.noreply.github.com>
2022-01-29 14:34:48 +0530

    product detail page started

--- lib/ProjectGalleryElements/ProjectCard.dart
+++ lib/ProjectGalleryElements/ProjectCard.dart
@@ -1,4 +1,5 @@
 import 'package:bennettprojectgallery/HomePageElements/GradientButton.dart';
+import 'package:bennettprojectgallery/project_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
@@ -18,7 +19,7 @@ class _ProjectCardState extends State<ProjectCard> {
         padding: EdgeInsets.all(0.0),
       ),
       onPressed: (){
-
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => ProjectDetail()));
       },
       onHover: (x){
         if(x){
